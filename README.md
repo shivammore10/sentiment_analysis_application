8dfd408064f910c1d6c777a007379eda154bdd4f
shivam <shivammore100@gmail.com>
2024-12-02 11:31:14 -0500

    Server file created

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sentiment_analysis_application
 sentiment analysis application using Watson NLP library
+This repo is for the practice project which is to be based on Embedded AI libraries. 
---
+++ server.py
@@ -0,0 +1,44 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : TODO
+# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+#Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    """
+    Analyze the sentiment of the given text and return the sentiment label and score.
+    """
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = sentiment_analyzer(text_to_analyze)
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    # Return a formatted string with the sentiment label and score
+    label_part = label.split('_')[1]
+    return f"The given text has been identified as {label_part} with a score of {score}."
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    #This function executes the Flask app and deploys it on localhost:5000.
+    app.run(host="0.0.0.0", port=5000)
